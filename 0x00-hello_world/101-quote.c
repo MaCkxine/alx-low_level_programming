871c72c351bc4a72ab356031df2d26ae9494b407
MaCkxine <maxine.gugu@gmail.com>
2023-03-16 19:50:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - prints the standrad error
  *
- * Return: 1
+ * Return: return 1
  */
-int man(void)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
