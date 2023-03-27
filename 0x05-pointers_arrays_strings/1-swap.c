d94f89b9e022b4d7b3d02551717f0e28b4165d56
MaCkxine <maxine.gugu@gmail.com>
2023-03-27 14:14:28 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - swap values of integer
+ * @a: swap integer
+ * @b: swap integer
+ * Return 0
+ */
+void swap_int(int *a, int *b);
+
+	int x;
+	{
+		x = *a;
+		*a = *b
+		*b = x;
+	}
