cf4def3f8c11c0ea2a32c45ec15e15bb15a58e63
MaCkxine <maxine.gugu@gmail.com>
2023-04-05 09:38:00 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,15 @@
 
 /**
  * _puts_recursion - function that prints a string
- * @s - input
+ * @s : input
  * Return - 0
  */
-
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
